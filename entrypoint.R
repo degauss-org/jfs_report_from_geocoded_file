33b7b6ae5f85d15b9b1610a2afa44e06c66322c9
Erika <erika.rasnick@gmail.com>
2020-05-04 15:32:27 -0400

    change column names to match JFS reports

--- entrypoint.R
+++ entrypoint.R
@@ -10,10 +10,13 @@ args <- parse_args(p)
 # args <- list()
 # args$file_name <- "sample_addresses_geocoded.csv"
 
+# INTAKE_ID
+# SCREENING_DECISION = SCREENED OUT or SCREENED IN
+# ALLEGATION_ADDRESS, formatted in a string without punctuation, it includes city, state, and zip code
 
 suppressPackageStartupMessages(library(readr))
 d <- read_csv(args$file_name,
-              col_types = cols(intake_id = col_character(),
+              col_types = cols(INTAKE_ID = col_character(),
                                fips_tract_id = col_character()))
 
 rmarkdown::render(input = '/app/generate_report.Rmd',
